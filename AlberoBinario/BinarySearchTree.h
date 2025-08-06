0faabb090e5ccd4e4b4fbf3809f62a46910f78ae
rickyruozzi <ruozziriccardo@4gmail.com>
2025-08-06 20:44:17 +0200

    inizio BST

---
+++ AlberoBinario/BinarySearchTree.h
@@ -0,0 +1,11 @@
+#ifndef BINARYSEARCHTREE 
+#define BINARYSEARCHTREE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+typedef struct treeNode{
+    int data;
+    treeNode* left;
+    treeNode* right;
+}treeNode;
+#endif
