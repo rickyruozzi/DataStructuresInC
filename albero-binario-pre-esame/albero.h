ec511ba0dd04b0dc9cf902a90993c7b25ab1e0e1
rickyruozzi <ruozziriccardo@4gmail.com>
2025-09-15 00:43:30 +0200

    nuovo makefile

---
+++ albero-binario-pre-esame/albero.h
@@ -0,0 +1,12 @@
+#ifndef ABPE
+#define ABPE
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
+typedef struct treeNode{
+    int val;
+    struct treeNode* left;
+    struct treeNode* right;
+}treeNode;
+
+#endif
